6b5abe732fd2fbb44bd5707aed62dbcafe61117d
Kacper Rutkowski <rutkovzky@gmail.com>
2021-02-20 13:18:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './styles/App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <p>elo</p>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Route exact path='/' component={HomeScreen} />
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Waluter</h1>
+      <ul>
+        <li>Ulubione</li>
+        <li>Info</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
